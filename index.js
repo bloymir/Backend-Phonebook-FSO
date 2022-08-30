2035c812fb3549ea1389b48efd9d5874ecad36d8
Nelson Tapia <ntapiapinto@gmail.com>
2022-08-30 00:42:50 -0400

    Se agrega variable entorno para PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT ||3001
 app.use(express.json())
 app.use(morgan( function (tokens, req, res ){
     return [
