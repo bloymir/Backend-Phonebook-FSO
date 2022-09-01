316b2408953e582175b57ec314ea91b2d2d4785a
Nelson Tapia <ntapiapinto@gmail.com>
2022-08-31 22:08:40 -0400

    3.19

--- index.js
+++ index.js
@@ -147,6 +147,8 @@ const errorHandler = (error, req, res, next) => {
 
   if(error.name === 'CastError'){
     return res.status(400).send({error: 'Malformatted id'})
+  } else if (error.name === 'ValidationError'){
+    return res.status(406).send({error: 'Name is used'})
   }
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.URL_MONGO_DB
 
@@ -11,7 +12,10 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        unique: true
+    },
     number: String
 })
 
@@ -22,5 +26,7 @@ personSchema.set('toJSON', {
       delete returnedObject.__v
     }
   })
+
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
